43580e243dbf208bab073b2ea9cd60d19b09076d
forBlank <1422848728@qq.com>
2020-12-21 22:34:25 +0800

    sync from api_doc

---
+++ model/model_apis.go
@@ -0,0 +1,20 @@
+package model
+
+type Apis struct {
+
+	UsersUrl string `json:"users_url,omitempty"`
+
+	UserUrl string `json:"user_url,omitempty"`
+
+	ArticlesUrl string `json:"articles_url,omitempty"`
+
+	ArticleUrl string `json:"article_url,omitempty"`
+
+	RepliesUrl string `json:"replies_url,omitempty"`
+
+	ReplyUrl string `json:"reply_url,omitempty"`
+
+	TagsUrl string `json:"tags_url,omitempty"`
+
+	TagUrl string `json:"tag_url,omitempty"`
+}
