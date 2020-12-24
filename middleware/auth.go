fc93b59a084792e5860552c042b0ec72fd9dc288
warpmatrix <warpmatrixlyk@gmail.com>
2020-12-24 09:38:36 +0800

    fix: for testing end

--- middleware/auth.go
+++ middleware/auth.go
@@ -5,7 +5,6 @@ import (
 	"github.com/qt-sc/server/lib"
 	"log"
 	"net/http"
-	"strings"
 )
 
 func Auth(next http.Handler) http.Handler{
@@ -13,7 +12,8 @@ func Auth(next http.Handler) http.Handler{
 		// TODO： 如果是login和signup，不需要鉴权
 		// 若有其他无需鉴权的api，请添加
 		// 为了做测试把/api/前缀api剔除
-		if r.URL.Path == "/users/login" || r.URL.Path == "/users/signup" || strings.HasPrefix(r.URL.Path, "/api/"){
+		// if r.URL.Path == "/users/login" || r.URL.Path == "/users/signup" || strings.HasPrefix(r.URL.Path, "/api/"){
+		if r.URL.Path == "/users/login" || r.URL.Path == "/users/signup"){
 			next.ServeHTTP(w, r)
 			return
 		}
